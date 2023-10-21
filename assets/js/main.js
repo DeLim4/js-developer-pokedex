3d7f1c3bb7f14ffe7f46d7b6b810113bca2de42a
Gustavi de Lima <gustavo.hygta03@gmail.com>
2023-10-21 18:29:22 -0300

    adição do switch modo claro/escuro

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,26 @@
+function clickMenu() {
+    if (menu.style.display == 'block') { menu.style.display = 'none' }
+    else { menu.style.display = 'block' }
+}
+
+
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+const body = document.querySelector("body")
+
+
+botaoAlterarTema.addEventListener("click", () => {
+
+    const modoEscuroAtivo = body.classList.contains("modo-escuro")
+    body.classList.add("modo-escuro");
+
+    if (modoEscuroAtivo) {
+        body.classList.remove("modo-escuro")
+    } else {
+        body.classList.add("modo-escuro")
+    }
+});
+
+
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 
